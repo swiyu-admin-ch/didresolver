a740452b8b156210b3508de692397d57b84e6c7d
Vladica Stojic BIT <vladica.stojic@bit.admin.ch>
2024-10-18 18:08:26 +0200

    Cleanup (no HTTP Get requests anymore). UDL Did interface remains unchanged. More idiomatic Rust.

--- src/did.rs
+++ src/did.rs
@@ -1,8 +1,12 @@
+use std::fmt::{Display, Formatter};
 // SPDX-License-Identifier: MIT
 use std::sync::Arc;
+use didtoolbox::did_tdw::{TrustDidWeb};
 use thiserror::Error;
-use crate::methods::{resolve_did_tdw, resolve_did_web, DidMethod};
+use crate::methods::{resolve_did_tdw};
 use didtoolbox::didtoolbox::DidDoc;
+use ssi::dids::{DIDBuf as SSIDIDSBuf, DIDMethod as SSIDIDSDIDMethod};
+use strum::{Display as EnumDisplay, AsRefStr as EnumAsRefStr, EnumString};
 
 #[derive(Error, Debug, PartialEq)]
 pub enum DidResolveError {
@@ -10,36 +14,59 @@ pub enum DidResolveError {
     DidNotSupported(String),
     #[error("the supplied did log is invalid: {0}")]
     InvalidDidLog(String),
-    #[error("a http error occurred while trying to read the did")]
-    HttpError(u16, String),
 }
+
+impl DidResolveError {
+    /// Returns the error kind.
+    pub fn kind(&self) -> DidResolveErrorKind {
+        match self {
+            Self::DidNotSupported(_) => DidResolveErrorKind::DidNotSupported,
+            Self::InvalidDidLog(_) => DidResolveErrorKind::InvalidDidLog,
+        }
+    }
+}
+
+/// DidResolveError kind.
+///
+/// Each [`DidResolveError`] variant has a kind provided by the [`DidResolveErrorKind::kind`] method.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum DidResolveErrorKind {
+    DidNotSupported,
+    InvalidDidLog,
+}
+
+#[derive(Debug, PartialEq, Default, EnumDisplay, EnumAsRefStr)]
+pub enum DidMethod {
+    #[strum(to_string = "tdw", serialize = "tdw")]
+    TDW,
+    #[default]
+    UNKNOWN,
+}
+
+#[derive(Debug, Default, PartialEq)]
+// TODO This struct resembles the ssi::dids::DID, which is a way more advanced, so refactoring would be perhaps a good idea.
 pub struct Did {
     pub parts: Vec<String>,
     method: DidMethod,
+    method_id: String,
 }
 
 impl Did {
     pub fn new(text: String) -> Self {
-        let mut method = DidMethod::UNKNOWN;
-        let splitted_did: Vec<String> = text.split(":").map(|x| x.to_string()).collect();
-
-        if splitted_did[0] == "did" {
-            method = match splitted_did[1].as_str() {
-                "web" => DidMethod::WEB,
-                "tdw" => DidMethod::TDW,
-                _ => DidMethod::UNKNOWN,
-            };
+        match Self::try_from(text.to_owned()) {
+            Ok(did) => did,
+            _ => {
+                Did { // fallback
+                    parts: text.split(":").map(|x| x.to_string()).collect(),
+                    method: DidMethod::default(),
+                    method_id: text.split(":").skip(2).map(|x| x.to_string()).collect(),
+                }
+            }
         }
-
-        return Did {
-            parts: splitted_did,
-            method: method,
-        };
     }
 
     pub fn resolve(&self, did_log_raw: String) -> Result<Arc<DidDoc>, DidResolveError> {
         let res = match self.method {
-            DidMethod::WEB => resolve_did_web(self),
             DidMethod::TDW => resolve_did_tdw(self, did_log_raw),
             DidMethod::UNKNOWN => Err(DidResolveError::DidNotSupported(String::new())),
         };
@@ -50,11 +77,45 @@ impl Did {
     }
 }
 
+/// This implementation reconstructs the original (textual) DID, regardless of its validity.
+impl Display for Did {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.parts.join(":"))
+    }
+}
+
+/// Implementation for a string denoting a did.
+impl TryFrom<String> for Did {
+    type Error = DidResolveError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match SSIDIDSBuf::try_from(value.to_owned()) {
+            Ok(buf) => {
+                if !buf.method_name().starts_with(TrustDidWeb::DID_METHOD_NAME) { // the single currently supported
+                    return Err(DidResolveError::DidNotSupported(buf.method_name().to_owned()));
+                };
+
+                let method_specific_id = buf.method_specific_id().to_string();
+
+                Ok(Did {
+                    parts: buf.split(":").map(|x| x.to_string()).collect(),
+                    method: DidMethod::TDW,
+                    method_id: method_specific_id,
+                })
+            }
+            Err(_e) => {
+                //eprintln!("{_e}");
+                Err(DidResolveError::DidNotSupported(value))
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
     use std::path::Path;
-    use crate::did::DidResolveError;
+    use crate::did::{DidMethod, DidResolveError, DidResolveErrorKind};
     use super::Did;
     use minify::json::minify;
     use rstest::{fixture, rstest};
@@ -72,52 +133,43 @@ mod tests {
         assert_eq!(did_doc.id, did_url);
     }
 
-    #[test]
-    fn resolve() {
-        // did.json is available at https://gist.github.com/bit-jniestroj/7fb3cce550db5a239b543035298429fe
-        let did_url = "did:web:gist.githubusercontent.com:bit-jniestroj:7fb3cce550db5a239b543035298429fe:raw:5e5540c6f67ffe30cca2dfc4bb950a68f412c406";
-
+    #[rstest]
+    // CAUTION A did_url (param #2) MUST match the one residing in did_log_raw_filepath (param #1)
+    #[case("test_data/did.jsonl", "did:tdw:E18kSmXiyfjSQfZgNdNnXi9JN7WADEfZycWvYXAu7VXAYso4CvifyEma7D3yhv7bmZ3cYHzgEQQTLq7rfuF7HjiY2fAi3:127.0.0.1%3A61646:123456789"
+    )]
+    // TODO Remove the should_panic attribute as soon as the error handling is properly done in didtoolbox
+    #[should_panic(expected = "Invalid did log. No entries found")]
+    fn resolve_did_tdw_invalid_did_log(#[case] _did_log_raw_filepath: String, #[case] did_url: String) {
         let did = Did::new(did_url.to_string());
 
-        did.resolve(String::new()).unwrap();
+        match did.resolve(String::new()) {
+            Err(e) => assert_eq!(e.kind(), DidResolveErrorKind::InvalidDidLog),
+            _ => (),
+        }
     }
 
-    #[test]
-    fn resolve_bbs() {
-        // did.json is available at https://gist.github.com/bit-jniestroj/f23a80ba382f94fdb93436424f6e4e01
-        let did_url = "did:web:gist.githubusercontent.com:bit-jniestroj:f23a80ba382f94fdb93436424f6e4e01:raw:d1bfa1f6c0bfee7687e9fc4ca6f86c2340c6b6f5";
+    #[rstest]
+    #[case("did:jwk:not_yet_supported")]
+    #[case("did:web:not_yet_supported")]
+    #[case("completely_irregular_did")]
+    fn did_not_supported(#[case] did_url: String) {
+        let did = Did::new(did_url.to_owned());
 
-        let did = Did::new(did_url.to_string());
-
-        did.resolve(String::new()).unwrap();
+        assert!(did.parts.len() > 0); // assuming none of the "#[rstest]" cases is empty
+        assert_eq!(did.to_string(), did_url); // assuming the Display trait is implemented accordingly for DID struct
+        assert_eq!(did.method, DidMethod::default());
+        assert!(did_url.ends_with(did.method_id.as_str()));
     }
 
-    #[test]
-    fn resolve_did_jwk() {
-        let did_url = "did:jwk:ansjdasd";
+    #[rstest]
+    #[case("did:jwk:not_yet_supported")]
+    #[case("did:web:not_yet_supported")]
+    fn resolve_did_not_supported(#[case] did_url: String) {
 
-        let did = Did::new(did_url.to_string());
-
-        // DID doc log is actually irrelevant for the test
-        let resolved = did.resolve(String::new());
+        // Actual DID log is pretty irrelevant for the test, so empty string would suffice
+        let resolved = Did::new(did_url).resolve(String::new());
 
         assert_eq!(resolved.is_err(), true);
         assert_eq!(resolved.err().unwrap(), DidResolveError::DidNotSupported(String::new()));
-    }
-
-    #[test]
-    fn resolve_did_wrong_url() {
-        let did_url = "did:web:example.com";
-
-        let did = Did::new(did_url.to_string());
-
-        // DID doc log is actually irrelevant for the test
-        let resolved = did.resolve(String::new());
-
-        assert_eq!(resolved.is_err(), true);
-        assert_eq!(
-            resolved.err().unwrap(),
-            DidResolveError::HttpError(404, "HTTP status client error (404 Not Found) for url (https://example.com/.well-known/did.json)".to_string())
-        );
     }
 }
--- src/methods.rs
+++ src/methods.rs
@@ -1,72 +1,23 @@
 // SPDX-License-Identifier: MIT
 use didtoolbox::didtoolbox::DidDoc;
-use didtoolbox::did_tdw::{TrustDidWebId, TrustDidWeb, DidMethodOperation};
-
+use didtoolbox::did_tdw::{TrustDidWebId, TrustDidWeb};
+use serde_json::from_str as json_from_str;
 use crate::did::{Did, DidResolveError};
 
-#[derive(PartialEq)]
-pub enum DidMethod {
-    WEB,
-    TDW,
-    UNKNOWN,
-}
-
 pub fn resolve_did_tdw(did: &Did, did_log_raw: String) -> Result<DidDoc, DidResolveError> {
-    let full_did = did.parts.join(":");
-    match TrustDidWebId::parse_did_tdw(full_did, Some(false)) {
+    match TrustDidWebId::parse_did_tdw(did.to_string(), Some(false)) {
         Ok(tdw_id) => {
-            let scid = tdw_id.get_scid();
-
-            let did_doc_json = match TrustDidWeb::read(scid, did_log_raw) {
+            let did_doc_json = match TrustDidWeb::read(tdw_id.get_scid(), did_log_raw) {
                 Ok(tdw) => tdw.get_did_doc(),
                 Err(e) => return Err(DidResolveError::InvalidDidLog(e.to_string())),
             };
 
-            match serde_json::from_str::<DidDoc>(&did_doc_json) {
-                Ok(doc) => Ok(doc),
-                Err(e) => {
-                    println!("Error parsing DID tdw document: {:?}", e);
-                    Err(DidResolveError::HttpError(500, String::from("Error parsing DID tdw document")))
-                }
-            }
+            // NOTE Calling unwrap is fully legit here, as TrustDidWeb::read(...) already ensures it is all well with the DID doc.
+            Ok(json_from_str::<DidDoc>(&did_doc_json).unwrap())
         }
-        Err(e) => Err(DidResolveError::DidNotSupported(e.to_string())),
-    }
-}
-
-pub fn resolve_did_web(did: &Did) -> Result<DidDoc, DidResolveError> {
-    let mut url = format!("https://{}", did.parts[2].replace("%3A", ":"));
-
-    if did.parts.len() > 3 {
-        let f = format!("/{}/did.json", did.parts[3..].join("/"));
-        url += &f;
-    } else {
-        url += "/.well-known/did.json";
-    }
-
-    let res = reqwest::blocking::get(url);
-
-    if !res.is_ok() {
-        let reqwest_error = res.err().unwrap();
-        return Err(DidResolveError::HttpError(
-            reqwest_error.status().unwrap().as_u16(),
-            reqwest_error.to_string(),
-        ));
-    }
-
-    let res = res.unwrap();
-
-    match res.error_for_status() {
-        Ok(_res) => {
-            let text = _res.text().unwrap();
-
-            let did_doc: DidDoc = serde_json::from_str(text.as_str()).unwrap();
-
-            Ok(did_doc.into())
-        }
-        Err(err) => Err(DidResolveError::HttpError(
-            err.status().unwrap().as_u16(),
-            err.to_string(),
-        )),
+        Err(e) => {
+            //eprintln!("{e}");
+            Err(DidResolveError::DidNotSupported(e.to_string()))
+        },
     }
 }
