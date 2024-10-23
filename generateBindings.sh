f703daac43e4459dae47cd66708f8d14c38a7a5f
Vladica Stojic BIT <vladica.stojic@bit.admin.ch>
2024-10-23 15:54:59 +0200

    Using didtoolbox 0.0.3

--- generateBindings.sh
+++
@@ -1,4 +0,0 @@
-cargo build --release
-cargo run --bin uniffi-bindgen generate --library target/release/libdidresolver.dylib --language kotlin --out-dir bindings/kotlin
-cargo run --bin uniffi-bindgen generate --library target/release/libdidresolver.dylib --language swift --out-dir bindings/swift/files
-cargo run --bin uniffi-bindgen generate --library target/release/libdidresolver.dylib --language python --out-dir bindings/python
--- kotlin-android-build.sh
+++ kotlin-android-build.sh
@@ -1,4 +1,4 @@
 cargo build --release
-# cargo install cargo-ndk ???
+# For details, see .github/workflows/build_kotlin-android.yml
 cargo ndk -o ./bindings/kotlin/jniLibs --manifest-path ./Cargo.toml -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64 build --release
 cargo run --bin uniffi-bindgen generate --library target/release/libdidresolver.so --language kotlin --out-dir bindings/kotlin
