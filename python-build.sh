f2005c377e0b1ca5879f0ec40fa5bcddb65cce7e
VladiStoBIT <vladica.stojic@bit.admin.ch>
2025-09-23 16:35:37 +0200

    EIDOMNI-161: ELF alignment check added (when building Kotlin bindings for Android)

--- python-build.sh
+++
@@ -1,8 +0,0 @@
-cargo build --release
-# CAUTION Ensure the both required "targets" (rustc --print target-list) are already installed e.g.
-# rustup target add x86_64-pc-windows-gnu x86_64-unknown-linux-gnu
-cargo build --release --target=x86_64-pc-windows-gnu
-TARGET_CC=x86_64-unknown-linux-gnu cargo build --release --target x86_64-unknown-linux-gnu
-cp target/release/libdidresolver.dylib bindings/python/
-cp target/x86_64-unknown-linux-gnu/release/libdidresolver.so bindings/python/
-cp target/x86_64-pc-windows-gnu/release/libdidresolver.dll bindings/python/
